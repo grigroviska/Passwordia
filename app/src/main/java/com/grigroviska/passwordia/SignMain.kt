1f7acdb465986654d4e44d9b01e30b2704cc71da
Grigroviska <karagozoglualper@protonmail.com>
2023-05-08 23:44:44 +0300

    Error fixed.

--- app/src/main/java/com/grigroviska/passwordia/SignMain.kt
+++ app/src/main/java/com/grigroviska/passwordia/SignMain.kt
@@ -2,13 +2,14 @@ package com.grigroviska.passwordia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.grigroviska.passwordia.Fragments.SignInEmail
 
 class SignMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_main)
 
-        val fragment = SignEmailFragment.newInstance()
+        val fragment = SignInEmail.newInstance()
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.frameLayout, fragment)
