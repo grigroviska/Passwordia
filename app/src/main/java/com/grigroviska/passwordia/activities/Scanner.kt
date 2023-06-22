8c6d7bf624ad5fb0c734d0178d437ef87ba44d2b
Grigroviska <karagozoglualper@protonmail.com>
2023-06-23 00:01:44 +0300

    A totp code is generated that changes every 30 seconds. Search with Account Name has been added in the search section. Data retrieval error in Scanner has been fixed.

--- app/src/main/java/com/grigroviska/passwordia/activities/Scanner.kt
+++ app/src/main/java/com/grigroviska/passwordia/activities/Scanner.kt
@@ -1,8 +1,10 @@
 package com.grigroviska.passwordia.activities
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.zxing.ResultPoint
 import com.grigroviska.passwordia.R
 import com.grigroviska.passwordia.databinding.ActivityScannerBinding
@@ -28,7 +30,7 @@ class Scanner : AppCompatActivity() {
             override fun barcodeResult(result: BarcodeResult?) {
                 result?.let {
                     val scannedCode = result.text
-                    returnScannedCode(scannedCode)
+                    handleScannedCode(scannedCode)
                 }
             }
 
@@ -47,11 +49,54 @@ class Scanner : AppCompatActivity() {
         barcodeScannerView.pause()
     }
 
-    private fun returnScannedCode(code: String) {
-        val resultIntent = Intent()
-        resultIntent.putExtra("scannedCode", code)
-        setResult(RESULT_OK, resultIntent)
-        finish()
+    private fun handleScannedCode(code: String) {
+        val otpData = extractOTPData(code)
+        otpData?.let {
+            val accountName = otpData.accountName
+            val secret = otpData.secret
+
+            if (accountName != null && secret != null) {
+                val resultIntent = Intent()
+                resultIntent.putExtra("accountName", accountName)
+                resultIntent.putExtra("secret", secret)
+                setResult(RESULT_OK, resultIntent)
+                finish()
+            } else {
+                showToast("Invalid QR code data")
+            }
+        } ?: run {
+            showToast("Invalid QR code data")
+        }
     }
 
+    private fun extractOTPData(code: String): OtpData? {
+        val uri = Uri.parse(code)
+        val issuer = extractAccountName(code)
+        val secret = uri.getQueryParameter("secret")
+        val accountName = "$issuer"
+
+        return if (!accountName.isNullOrBlank() && !secret.isNullOrBlank()) {
+            OtpData(accountName, secret)
+        } else {
+            null
+        }
+    }
+
+    private data class OtpData(
+        val accountName: String?,
+        val secret: String?
+    )
+
+    private fun extractAccountName(code: String): String? {
+        val start = code.indexOf("totp/") + 5
+        val end = code.indexOf("?", start)
+        if (start != -1 && end != -1 && end > start) {
+            return code.substring(start, end)
+        }
+        return null
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this@Scanner, message, Toast.LENGTH_SHORT).show()
+    }
 }
